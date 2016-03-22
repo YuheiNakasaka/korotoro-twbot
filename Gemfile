5b8f90a875794f67fa81fa64352ac5bd25a9425c
YuheiNakasaka <yuhei.nakasaka@gmail.com>
2016-03-22 18:00:43 +0900

    init

---
+++ Gemfile
@@ -0,0 +1,6 @@
+source 'https://rubygems.org'
+gem 'docomoru'
+gem 'twitter'
+group :development, :test do
+  gem 'pry'
+end
---
+++ bot.rb
@@ -0,0 +1,78 @@
+# coding: utf-8
+require 'yaml'
+require 'twitter'
+require 'docomoru'
+require 'pry'
+
+class Bot
+  attr_accessor :client, :timeline
+  def initialize
+    env = YAML.load_file('./config.yml')
+    timeline = Twitter::Streaming::Client.new do |config|
+      config.consumer_key        = env['consumer_key']
+      config.consumer_secret     = env['consumer_secret']
+      config.access_token        = env['access_token']
+      config.access_token_secret = env['access_token_secret']
+    end
+
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = env['consumer_key']
+      config.consumer_secret     = env['consumer_secret']
+      config.access_token        = env['access_token']
+      config.access_token_secret = env['access_token_secret']
+    end
+
+    @timeline = timeline
+    @client = client
+  end
+
+  def post(msg = '', tw_id: nil, status_id: nil)
+    if status_id
+      rep_msg = "@#{tw_id} #{msg}"
+      @client.update(rep_msg, {in_reply_to_status_id: status_id})
+      puts "#{rep_msg}"
+    else
+      @client.update(msg)
+      puts "#{msg}"
+    end
+  end
+end
+
+class ReplyEngine
+  def initialize(text='')
+    @text = text
+    env = YAML.load_file('./config.yml')
+    @client = Docomoru::Client.new(api_key: env["docomo_api_key"])
+  end
+
+  def result
+    @client.create_dialogue(@text)
+  end
+end
+
+bot = Bot.new
+begin
+  bot.timeline.user do |tw|
+    case tw
+    when Twitter::Tweet
+      text = tw.text
+      twitter_id = tw.user.screen_name
+      status_id = tw.id
+      # Not RT and only reply to @korot0ro
+      if !text.index("RT")
+        if text.match(/^@korot0ro/)
+          rep_engine = ReplyEngine.new(text.sub(/^@korot0ro /,''))
+          reply_msg = rep_engine.result.body["utt"]
+          bot.post(reply_msg, tw_id: twitter_id, status_id: status_id)
+          sleep 3
+        end
+      end
+    end
+  end
+rescue => e
+  puts "#{Time.now}: #{e}"
+  sleep 3
+  retry
+rescue Interrupt
+  exit 1
+end
